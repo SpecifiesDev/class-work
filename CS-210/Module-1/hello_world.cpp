bf9b24fcf20a93fa1bf77e27418c97d8b7a38bee
Austin <38254442+SpecifiesDev@users.noreply.github.com>
2024-03-09 20:03:57 -0500

    push module-1 work

---
+++ CS-210/Module-1/hello_world.cpp
@@ -0,0 +1,17 @@
+// Developer: Dallas Gable
+// Date: March 9, 2024
+// Purpose: Output "Hello, World!" to the console
+
+// Include iosstream and standard namespace.
+#include <iostream> 
+using namespace std; 
+
+// entrypoint function of the program
+int main() {
+
+    // use char out to print "Hello, World!" to the console, followed by a newline.
+    cout << "Hello, World!" << endl; 
+
+    // return default value of main function.
+    return 0;
+}
---
+++ CS-210/Module-1/hello_world.py
@@ -0,0 +1,11 @@
+# Developer: Dallas Gable
+# Date: March 9, 2024
+# Purpose: Output "Hello, World!" to the console
+
+# create a functon to output "Hello, World!" to the console
+def main():
+    print("Hello, World!")
+
+# make sure python knows to run the main function
+if __name__ == "__main__":
+    main()
